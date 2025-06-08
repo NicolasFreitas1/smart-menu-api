f61a66c1ca1b1082042150fef46aba545db430f7
Nicolas Freitas <nicolasandrade.de.freitas@gmail.com>
2025-06-08 17:04:02 -0300

    fix: pagination returns

---
+++ src/infra/http/controllers/dish/list-dishes-by-restaurant.controller.ts
@@ -0,0 +1,64 @@
+import { ListDishesByRestaurantUseCase } from '@/domain/smart-menu/application/use-cases/dish/list-dishes-by-restaurant'
+import { Public } from '@/infra/auth/public'
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Param,
+  ParseUUIDPipe,
+  Query,
+} from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '../../pipes/zod-validation-pipe'
+import { DishWithPaginationPresenter } from '../../presenters/dish-with-pagination-presenter'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+type pageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+const sizeQueryParamSchema = z
+  .string()
+  .optional()
+  .default('20')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+const sizeValidationPipe = new ZodValidationPipe(sizeQueryParamSchema)
+
+type sizeQueryParamSchema = z.infer<typeof sizeQueryParamSchema>
+
+@Controller('dishes/restaurant/:restaurantId')
+export class ListDishesByRestaurantController {
+  constructor(private listDishesUseCase: ListDishesByRestaurantUseCase) {}
+
+  @Get()
+  @Public()
+  async handle(
+    @Query('page', queryValidationPipe) page: pageQueryParamSchema,
+    @Query('per_page', sizeValidationPipe) perPage: sizeQueryParamSchema,
+    @Param('restaurantId', ParseUUIDPipe) restaurantId: string,
+  ) {
+    const result = await this.listDishesUseCase.execute({
+      restaurantId,
+      page,
+      perPage,
+    })
+
+    if (result.isLeft()) {
+      console.log(result)
+
+      throw new InternalServerErrorException('Something went wrong')
+    }
+
+    const dishes = result.value.dishes
+
+    return DishWithPaginationPresenter.toHTTP(dishes)
+  }
+}
