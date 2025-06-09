c09889309ca5537e0d42e5e2296cfebb1bd8d865
Nicolas Freitas <nicolasandrade.de.freitas@gmail.com>
2025-06-08 23:06:18 -0300

    fix: user return list

--- src/infra/http/presenters/user-with-pagination-presenter.ts
+++ src/infra/http/presenters/user-with-pagination-presenter.ts
@@ -1,15 +1,15 @@
-import { DataWithPagination } from '@/core/repositories/data-with-pagination'
-import { User } from '@/domain/smart-menu/enterprise/entities/user'
-import { UserPresenter } from './user-presenter'
+import { DataWithPagination } from "@/core/repositories/data-with-pagination";
+import { User } from "@/domain/smart-menu/enterprise/entities/user";
+import { UserPresenter } from "./user-presenter";
 
 export class UserWithPaginationPresenter {
   static toHTTP(user: DataWithPagination<User>) {
     return {
-      users: user.data.map(UserPresenter.toHTTP),
+      data: user.data.map(UserPresenter.toHTTP),
       amount: user.amount,
       totalPages: user.totalPages,
       actualPage: user.actualPage,
       perPage: user.perPage,
-    }
+    };
   }
 }
