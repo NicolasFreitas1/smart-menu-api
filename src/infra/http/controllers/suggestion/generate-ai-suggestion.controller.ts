9daccfc7c5a1e387883d4acf3cb019b06e323304
Nicolas Freitas <nicolasandrade.de.freitas@gmail.com>
2025-06-10 13:55:09 -0300

    fix: remove unused vars

--- src/infra/http/controllers/suggestion/generate-ai-suggestion.controller.ts
+++ src/infra/http/controllers/suggestion/generate-ai-suggestion.controller.ts
@@ -1,22 +1,10 @@
-import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error';
-import { GetDishByIdUseCase } from '@/domain/smart-menu/application/use-cases/dish/get-dish-by-id';
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  Get,
-  NotFoundException,
-  Param,
-  ParseUUIDPipe,
-  Post,
-} from '@nestjs/common';
-import { DishPresenter } from '../../presenters/dish-presenter';
-import { Public } from '@/infra/auth/public';
-import { GenerateAISuggestionUseCase } from '@/domain/smart-menu/application/use-cases/ai/generate-ai-suggestion';
+import { GenerateAISuggestionUseCase } from '@/domain/smart-menu/application/use-cases/ai/generate-ai-suggestion'
+import { Public } from '@/infra/auth/public'
+import { Body, Controller, Param, ParseUUIDPipe, Post } from '@nestjs/common'
 import {
   GenerateAISuggestionSchema,
   bodyValidationPipe,
-} from './dtos/message-sent.dto';
+} from './dtos/message-sent.dto'
 
 @Controller('ai/suggestion/:restaurantId')
 export class GenerateAISuggestionController {
@@ -30,27 +18,15 @@ export class GenerateAISuggestionController {
     @Param('restaurantId', ParseUUIDPipe) restaurantId: string,
     @Body(bodyValidationPipe) body: GenerateAISuggestionSchema,
   ) {
-    const { messages } = body;
+    const { messages } = body
 
     const result = await this.generateAISuggestionUseCase.execute({
       messages,
       restaurantId,
-    });
-
-    // if (result.isLeft()) {
-    //   const error = result.value;
-
-    //   switch (error.constructor) {
-    //     case ResourceNotFoundError:
-    //       throw new NotFoundException(error.message);
-
-    //     default:
-    //       throw new BadRequestException(error.message);
-    //   }
-    // }
+    })
 
     return {
       message: result,
-    };
+    }
   }
 }
