348a67b079d5d83f2d4a48425f1eecb726d81957
Nicolas Freitas <nicolasandrade.de.freitas@gmail.com>
2025-06-10 08:48:39 -0300

    feat: add environment variables

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -6,6 +6,7 @@ export const envSchema = z.object({
   JWT_PUBLIC_KEY: z.string(),
   PORT: z.coerce.number().optional().default(3333),
   OPENAI_API_KEY: z.string(),
+  GOOGLE_GENERATIVE_AI_API_KEY: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
