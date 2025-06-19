06e7a25dae5257935084d3f75b47fb272b7e17b0
Nicolas Freitas <nicolasandrade.de.freitas@gmail.com>
2025-06-19 19:29:47 -0300

    feat: price in suggestion

--- src/infra/http/controllers/suggestion/dtos/message-sent.dto.ts
+++ src/infra/http/controllers/suggestion/dtos/message-sent.dto.ts
@@ -1,21 +1,21 @@
-import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe';
-import { z } from 'zod';
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { z } from 'zod'
 
 export const messageSchema = z.object({
   role: z.enum(['user', 'assistant']),
   content: z.string().min(1),
-});
+})
 
 export const generateAISuggestionSchema = z.object({
   messages: z.array(messageSchema).min(1),
-});
+})
 
 // Tipagem TypeScript baseada no schema
-export type MessageSent = z.infer<typeof messageSchema>;
+export type MessageSent = z.infer<typeof messageSchema>
 export type GenerateAISuggestionSchema = z.infer<
   typeof generateAISuggestionSchema
->;
+>
 
 export const bodyValidationPipe = new ZodValidationPipe(
   generateAISuggestionSchema,
-);
+)
